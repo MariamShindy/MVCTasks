d06455ab8764d7a476c5104c258185ae0bcdd45f
MariamShindy <125688137+MariamShindy@users.noreply.github.com>
2024-03-19 16:41:43 +0200

    DAL For Department Module

---
+++ TaskThree.DA/Data/ApplicationDBContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using TaskThree.DA.Models;
+
+namespace TaskThree.DA.Data
+{
+    internal class ApplicationDBContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseSqlServer("Server=.; Database= MVCApplication; Trusted_Connection= True;");
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
+        public DbSet <Department> Departments { get; set; }
+    }
+}
---
+++ TaskThree.DA/Models/Department.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskThree.DA.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        //[Required(ErrorMessage ="code is reuqired here !!")]
+        public string Code { get; set; }
+        //[Required]
+        public string Name { get; set; }
+        public DateTime DateOfCreation { get; set; }
+    }
+}
