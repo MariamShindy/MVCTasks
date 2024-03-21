dcfe8529c208e42d8a4676fa84583c4063c01730
MariamShindy <125688137+MariamShindy@users.noreply.github.com>
2024-03-21 11:07:59 +0200

    Business Logic Layer

---
+++ TaskThree.BLL/Repositories/DepartmentRepository.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskThree.BLL.Interfaces;
+using TaskThree.DA.Data;
+using TaskThree.DA.Models;
+
+namespace TaskThree.BLL.Repositories
+{
+    internal class DepartmentRepository : IDepartmentRepository
+    {
+        private readonly ApplicationDBContext _dbContext;
+        public DepartmentRepository(ApplicationDBContext dbContext)
+        {
+            _dbContext = dbContext;//Ask CLR to create the dbcontext object 
+            /*new ApplicationDBContext(new DbContextOptions<ApplicationDBContext>())*/
+        }
+        public int Add(Department entity)
+        {
+            _dbContext.Departments.Add(entity);
+            return _dbContext.SaveChanges();
+        }
+
+        public int Delete(Department entity)
+        {
+            _dbContext.Departments.Remove(entity);
+            return _dbContext.SaveChanges();
+        }
+
+        public Department Get(int id)
+        {
+            //return _dbContext.Departments.Find(id);
+            return _dbContext.Find<Department>(id);
+        }
+
+        public IEnumerable<Department> GetAll()
+        {
+            return _dbContext.Departments.AsNoTracking().ToList();
+        }
+
+        public int Update(Department entity)
+        {
+            _dbContext.Departments.Update(entity);
+            return _dbContext.SaveChanges();
+        }
+    }
+}
