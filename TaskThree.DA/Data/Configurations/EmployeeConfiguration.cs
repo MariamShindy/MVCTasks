2d855dd6abc824402e203441d4c629146857aa5e
MariamShindy <125688137+MariamShindy@users.noreply.github.com>
2024-03-23 02:06:05 +0200

    Employee DAL

---
+++ TaskThree.DA/Data/Configurations/EmployeeConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskThree.DA.Models;
+
+namespace TaskThree.DA.Data.Configurations
+{
+    internal class EmployeeConfiguration : IEntityTypeConfiguration<Employee>
+    {
+        public void Configure(EntityTypeBuilder<Employee> builder)
+        {
+            builder.Property(E => E.Name).HasColumnType("varchar").HasMaxLength(50).IsRequired();
+            builder.Property(E=>E.Address).IsRequired();
+            builder.Property(E => E.Salary).HasColumnType("decimal(12 ,2)");
+            builder.Property(E => E.Gender).HasConversion(
+                (Gender) => Gender.ToString(),
+                (gender) => (Gender)Enum.Parse(typeof(Gender), gender, true));
+        }
+    }
+}
---
+++ TaskThree.DA/Models/Employee.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskThree.DA.Models
+{
+    public enum Gender
+    {
+        [EnumMember(Value ="Male")]
+        Male = 1,
+        [EnumMember(Value = "Female")]
+
+        Female = 2
+    }
+    public enum EmpType
+    {
+        FullTime = 1,
+        PartTime = 2
+    }
+    public class Employee
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Name is required")]
+        [MaxLength(50,ErrorMessage = "Max length of name is 50 chars")]
+        [MinLength(5, ErrorMessage = "Min length of name is 5 chars")]
+        public string Name { get; set; }
+        [Range(22,30)]
+        public int? Age { get; set; }
+        [RegularExpression(@"^[0-9]{1,3}-[a-zA-Z]{5,10}-[a-zA-Z]{5,10}-[a-zA-Z]{4,10}-[a-zA-Z]{5,10}$",ErrorMessage ="Address must be like 123 Street-City-Country")]
+        public string Address {  get; set; }
+        public EmpType EmployeeType { get; set; }
+        public Gender Gender { get; set; }
+        [DataType(DataType.Currency)]
+        public decimal Salary { get; set; }
+        [Display(Name = "IsActive")]
+        public bool isActive;
+        [EmailAddress]
+        public string Email { get; set; }
+        [Display(Name = "Phone Number")]
+        public string PhoneNumber { get; set; }
+        [Display(Name = "Hiring Date")]
+        public DateTime HiringDate { get; set; }
+        public DateTime CreationDate { get; set; } = DateTime.Now;
+
+
+
+
+
+    }
+}
