c124c451f78c01abcbd4c48baf761bc3894cb6db
MariamShindy <125688137+MariamShindy@users.noreply.github.com>
2024-03-24 01:15:38 +0200

    EmployeeRepository - BLL

---
+++ TaskThree.BLL/Repositories/EmployeeRepository.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskThree.BLL.Interfaces;
+using TaskThree.DA.Data;
+using TaskThree.DA.Models;
+
+namespace TaskThree.BLL.Repositories
+{
+    public class EmployeeRepository:IEmployeeRepository
+    {
+        private readonly ApplicationDBContext _dbContext;
+        public EmployeeRepository(ApplicationDBContext dbContext)
+        {
+            _dbContext = dbContext;//Ask CLR to create the dbcontext object 
+        }
+        public int Add(Employee entity)
+        {
+            _dbContext.Employees.Add(entity);
+            return _dbContext.SaveChanges();
+        }
+
+        public int Delete(Employee entity)
+        {
+            _dbContext.Employees.Remove(entity);
+            return _dbContext.SaveChanges();
+        }
+
+        public Employee Get(int id)
+        {
+            return _dbContext.Find<Employee>(id);
+        }
+
+        public IEnumerable<Employee> GetAll()
+        {
+            return _dbContext.Employees.AsNoTracking().ToList();
+        }
+
+        public int Update(Employee entity)
+        {
+            _dbContext.Employees.Update(entity);
+            return _dbContext.SaveChanges();
+        }
+
+    }
+}
