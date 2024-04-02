714f987b472efef33a00e27568c72bf133df24cd
MariamShindy <125688137+MariamShindy@users.noreply.github.com>
2024-04-02 15:36:43 +0200

    Document Settings

---
+++ TaskThree.PL/Helpers/DocumentSettings.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.IO;
+
+namespace TaskThree.PL.Helpers
+{
+    public static class DocumentSettings
+    {
+        public static string UploadFile(IFormFile file, string FolderName)
+        {
+            //string FolderPath = $"{Directory.GetCurrentDirectory()}\\wwwroot\\files\\{FolderName}";
+            string FolderPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\files", FolderName);
+            if (!Directory.Exists(FolderPath))
+            {
+                Directory.CreateDirectory(FolderPath);
+            }
+            string FileName = $"{Guid.NewGuid()} {Path.GetExtension(file.FileName)}";
+
+            string FilePath = Path.Combine(FolderPath, FileName);
+            using var fileStream = new FileStream(FilePath, FileMode.Create);
+            file.CopyTo(fileStream);
+            return FileName;
+        }
+        public static void DeleteFile(string FileName , string FolderName)
+        {
+            string FilePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\files" ,FolderName, FileName);
+            if (File.Exists(FilePath))
+                File.Delete(FilePath);
+        }
+    }
+}
